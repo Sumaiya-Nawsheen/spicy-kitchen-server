8672763d978bc9e6bae26a43b36ac3f97ed7288b
Sumaiya-Nawsheen <sumaiya.naw252@gmail.com>
2021-04-08 23:01:06 +0600

    spicy-kitchen-server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
+const  ObjectId  = require('mongodb').ObjectID;
 require('dotenv').config();
 
 
@@ -21,9 +22,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    console.log("connection error", err);
+    
   const foodCollection = client.db("spicy-kitchen").collection("foodItems");
+  const customersCollection = client.db("spicy-kitchen").collection("customer");
  
+
 app.get('/foodItems', (req,res) => {
   foodCollection.find()
   .toArray((err, items) => {
@@ -42,6 +45,37 @@ app.get('/foodItems', (req,res) => {
        res.send(result.insertedCount>0);
     })
   })
+
+  app.get('/checkout/:id', (req,res) => {
+    foodCollection.find({_id: ObjectId (req.params.id)})
+    .toArray((err, items) => {
+      console.log(items)
+      res.send(items[0]);
+      
+    })
+  })
+
+  app.post('/addOrders', (req, res) => {
+    const orderedItem = req.body;
+    customersCollection.insertOne(orderedItem)
+    .then (result => {
+      res.send(result.insertedCount>0);
+      console.log(result.insertedCount)
+    })
+    console.log(orderedItem)
+  })
+
+  app.get('/totalOrders', (req,res) => {
+    console.log(req.query.email);
+    customersCollection.find({email: req.query.email})
+    .toArray((err, items) => {
+      console.log(items)
+      res.send(items);
+      
+    })
+  })
+
+
 //   client.close();
 });
 
